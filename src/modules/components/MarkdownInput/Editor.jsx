8a66d1077e4df026e8c3d7934b1d58b6545eed77
Troy Cosentino <troycosentino@gmail.com>
2018-04-20 09:49:32 -0700

    v1

--- src/modules/components/MarkdownInput/Editor.jsx
+++ src/modules/components/MarkdownInput/Editor.jsx
@@ -75,7 +75,6 @@ class OurEditor extends React.Component {
   onChange(editorState) {
     const md = stateToMarkdown(editorState.getCurrentContent());
 
-    console.log(md.substr(0, md.length - 1));
     this.props.input.onChange(md.substr(0, md.length - 1));
     this.setState({ editorState });
   }
