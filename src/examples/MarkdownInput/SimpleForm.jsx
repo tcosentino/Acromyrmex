a46e8a89c852a886dd3af248705a97af842b1271
Troy Cosentino <troycosentino@gmail.com>
2019-11-19 09:25:56 -0800

    removed most form fields

--- src/examples/MarkdownInput/SimpleForm.jsx
+++ src/examples/MarkdownInput/SimpleForm.jsx
@@ -3,11 +3,6 @@ import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
 import MarkdownInput from '../../modules/components/MarkdownInput';
 import startingOptions from './options';
-import TemplateInput from '../../modules/components/TemplateInput';
-import SelectInput from '../../modules/components/SelectInput';
-import DateInput from '../../modules/components/DateInput';
-import TextInput from '../../modules/components/TextInput';
-import FormulaInput from '../../modules/components/FormulaInput';
 
 class SimpleForm extends React.Component {
   constructor(props) {
@@ -32,127 +27,6 @@ class SimpleForm extends React.Component {
     return (
       <form onSubmit={handleSubmit}>
         <div>
-          <Field name="text" label="Text" component={TextInput} />
-        </div>
-        <div>
-          <Field
-            name="select"
-            label="Select"
-            component={SelectInput}
-            options={[{ id: '1', name: 'option 1' }, { id: '2', name: 'option 2' }]}
-          />
-        </div>
-        <div>
-          <Field name="template" label="Template" component={TemplateInput} options={options} />
-        </div>
-        <div>
-          <Field
-            name="frequency"
-            component={SelectInput}
-            label="Frequency"
-            multi
-            options={[
-              {
-                id: 'w',
-                name: 'weekly'
-              },
-              {
-                id: 'd',
-                name: 'daily'
-              },
-              {
-                id: 'h',
-                name: 'hourly'
-              }
-            ]}
-          />
-        </div>
-        <div>
-          <Field
-            name="empty-template"
-            label="Empty Template"
-            component={TemplateInput}
-            options={[]}
-          />
-        </div>
-        <div>
-          <Field
-            name="equation"
-            label="Equation"
-            component={TemplateInput}
-            options={[{ id: 'name', name: 'Name' }, { id: 'phone-number', name: 'Phone Number' }]}
-            fixOptions={o => ({ ...o, textValue: `{${o.id}}` })}
-          />
-        </div>
-        {/* <div>
-          <Field name="datetime" label="DateTime Input" component={DateInput} />
-        </div>
-        <div>
-          <Field name="date" label="Date Input" component={DateInput} showTimeSelect={false} />
-        </div>
-        <div>
-          <Field
-            name="time"
-            label="Time Input"
-            component={DateInput}
-            showTimeSelect
-            showDateSelect={false}
-          />
-        </div> */}
-        <div>
-          <Field name="datetime-clearable" label="Date Input clearable" component={DateInput} />
-        </div>
-        {/* <div>
-          <Field
-            name="datetime-options"
-            label="Date Input w/ options"
-            component={DateInput}
-            showTimeSelect
-            options={options}
-          />
-        </div>
-        <div>
-          <Field
-            name="date-options.value"
-            label="Date Input w/ options"
-            component={DateInput}
-            showTimeSelect={false}
-            options={options}
-            addonCustomBefore={
-              <Field
-                name={'date-options.queryType'}
-                component={SelectInput}
-                disabled
-                label={'Type of filter'}
-                options={[{ id: '0', name: '=' }]}
-                addon
-              />
-            }
-          />
-        </div> */}
-        <div>
-          <Field
-            name="select-input"
-            label="Select Input"
-            component={SelectInput}
-            options={[{ id: '1', name: 'true' }, { id: '2', name: 'false' }]}
-            templateOptions={options}
-            addonCustomBefore={
-              <Field
-                name="date-options.queryType"
-                component={SelectInput}
-                disabled
-                label="Type of filter"
-                options={[{ id: '0', name: '=' }]}
-                addon
-              />
-            }
-          />
-        </div>
-        <div>
-          <Field name="markdown" label="Markdown" component={MarkdownInput} options={options} />
-        </div>
-        <div>
           <Field
             name="plainMarkdown"
             label="Plain Markdown"
@@ -163,18 +37,11 @@ class SimpleForm extends React.Component {
         </div>
         <div>
           <Field
-            name="formula"
-            label="Formula"
-            component={FormulaInput}
-            attributes={[
-              { id: 'employee-count', name: 'Employee Count', textValue: '#employee-count#' },
-              { id: 'name', name: 'Name', textValue: '#name#' }
-            ]}
-            formulas={[
-              { id: 'count', name: 'Count', textValue: '$COUNT$' },
-              { id: 'sum', name: 'Sum', textValue: '$SUM$' }
-            ]}
+            name="plainMarkdown2"
+            label="Plain Markdown 2"
+            component={MarkdownInput}
             options={options}
+            plainText
           />
         </div>
         <div>
@@ -201,13 +68,7 @@ SimpleForm.propTypes = {
 export default reduxForm({
   form: 'simple', // a unique identifier for this form
   initialValues: {
-    formula: '$COUNT$(" #employee-count# = {dfasdferwegsf.replacement-workstation}")',
     plainMarkdown:
-      'sdfadf [asdfasdf](fasdfasf) adsfasdf [fasdf432](1231sadf) {dfasdfsdg3q34t.ip-address} {dfasdfsdg3q34t.mac-address} dsfs ',
-    markdown:
-      'sdfadf [asdfasdf](fasdfasf) adsfasdf [fasdf432](1231sadf) {dfasdfsdg3q34t.ip-address} {dfasdfsdg3q34t.mac-address} dsfs ',
-    'date-options': {
-      value: '2018-05-24T05:12:08.848Z'
-    }
+      'sdfadf [asdfasdf](fasdfasf) adsfasdf [fasdf432](1231sadf) {dfasdfsdg3q34t.ip-address} {dfasdfsdg3q34t.mac-address} dsfs '
   }
 })(SimpleForm);
